40038a694ebf7b3e83b8dad843332644f3454e72
chensihua <kushiyou@itcast.cn>
2025-07-05 19:27:51 +0800

    add chanikLink/env-enc and and verify function to verify sepolia address

--- HardHat/hardhat.config.ts
+++ HardHat/hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+/* import envEnc from "@chainLink/env-enc";
+console.log("env_enc", envEnc); */
+require("@chainLink/env-enc").config();
+
+const {SEPOLIA_URL,PRIVATE_KEY} = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia:{
+      url:SEPOLIA_URL,
+      accounts:[PRIVATE_KEY as `string`],
+    }
+  }
 };
 
 export default config;
--- HardHat/script/DeployFundMe.ts
+++ HardHat/script/DeployFundMe.ts
@@ -1,4 +1,7 @@
+import { fundMeSol } from "../typechain-types";
+
 const { ethers } = require("hardhat");
+const hre = require("hardhat")
 
 async function main() {
     const fundMeFactory = await ethers.getContractFactory("FundMe");//传入的文件名字是contract文件夹下的合约的名字
@@ -9,6 +12,18 @@ async function main() {
     const fundMe = await fundMeFactory.deploy(10);//这个时候调用fundMe.sol合约的constructor函数
     await fundMe.waitForDeployment();
     console.log("FundMe contract deployed to:", await fundMe.getAddress());
+    //等待五个区块确认
+    await fundMe.deployTransaction.wait(5);
+    console.log("Waiting for 5 blocks confirmation...");
+    //验证sepolia地址
+    await verifyFundMe(fundMe,10)
+}
+ 
+async function verifyFundMe(fundMe:any,arg:number){
+    await hre.run("verfy:verify",{
+        address: fundMe.target,
+        constructorArguments:[arg]
+    })
 }
 
 // Execute the main function and handle errors
