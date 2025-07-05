fc52eaea5be0e1009d3505084de5c06c6ceda1a2
chensihua <kushiyou@itcast.cn>
2025-07-06 00:16:08 +0800

    add secondAccount and add simple transfer contract

--- HardHat/hardhat.config.ts
+++ HardHat/hardhat.config.ts
@@ -4,14 +4,18 @@ import "@nomicfoundation/hardhat-toolbox";
 console.log("env_enc", envEnc); */
 require("@chainLink/env-enc").config();
 
-const {SEPOLIA_URL,PRIVATE_KEY} = process.env;
+const {
+  SEPOLIA_URL,
+  PRIVATE_KEY,
+  PRIVATE_KEY_1
+} = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
     sepolia:{
       url:SEPOLIA_URL,
-      accounts:[PRIVATE_KEY as `string`],
+      accounts:[PRIVATE_KEY as `string`,PRIVATE_KEY_1 as `string`],
       chainId:11155111,
     }
   }
