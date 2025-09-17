b39a5f5233e9188b04b4a623b6a78664bdc4e724
chensihua <kushiyou@itcast.cn>
2025-09-17 18:17:02 +0800

    add solc to compile solidity and use Ganache build local blockchain with use ethers.js to connect it

---
+++ LessonCode/Lesson05/depoly.js
@@ -0,0 +1,28 @@
+import {ethers} from "ethers";
+import fs from "fs";
+import path from "path";
+
+console.log(ethers);
+
+
+async function main () {
+    //链接到区块链的地址
+    const provider = new ethers.provider.JsonRpcProvider("http://0.0.0.0:8545");
+    //获取钱包私钥，这里的0x0x需要替换成自己的私钥
+    const wallet = new ethers.Wallet("0x0x",provider);
+    //读取编译的abi和bin文件
+    const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi','utf8');
+    const binary = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.bin','utf8');
+    //创建合约工厂用于部署合约，需要传入abi，bin和钱包信息
+    const contractFactory = new ethers.ContractFactory(abi,binary,wallet);
+    console.log("Deploying, please wait...");
+    //部署合约并等待返回状态
+    const contract = await contractFactory.deploy();
+}
+
+main().then(()=>{
+    process.exit(0)
+}).catch((error)=>{
+    console.error(error);
+    process.exit(1);
+})
