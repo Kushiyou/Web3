1c98153380bce78e11a4b3e2200bafc6e2c655d8
chensihua <kushiyou@itcast.cn>
2025-10-17 01:07:10 +0800

    react version connect MetaMask

--- LessonCode/Lesson08/React_M/src/App.tsx
+++ LessonCode/Lesson08/React_M/src/App.tsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-
+import {useEffect} from 'react'
 import './App.css'
+import Home from './Layout/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const isWindowMetaMaskInstalled = ()=>{
+    return typeof (window as any).ethereum !== 'undefined' && (window as any).ethereum.isMetaMask
+  }
+
+  useEffect(()=>{
+    if(isWindowMetaMaskInstalled()){
+      console.log("MetaMask is installed!")
+    }else{
+      console.log("MetaMask is not installed!")
+    }
+  },[])
 
   return (
-    <>
-      
-    </>
+    <div style={{width:"100vw",height:"100vh"}}>
+      {
+        isWindowMetaMaskInstalled()?
+        <Home /> : 
+        <h1 style={
+          {margin: '25rem', textAlign:'center', color:'red',fontSize:'3rem'}
+        }> 请安装 MetaMask </h1>
+      }  
+    </div>
   )
 }
 
--- LessonCode/Lesson08/React_M/src/Layout/Home.tsx
+++ LessonCode/Lesson08/React_M/src/Layout/Home.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import {useState} from 'react'
+import SplitText from "../Componet/SplitText";
 
 export default function Home() {
+  const [loginState,setLoginState] = useState(false)
+
+  const connectMetaMask = async ()=>{
+    
+    if(typeof (window as any).ethereum !== 'undefined' && (window as any).ethereum.isMetaMask){
+        const accounts = await (window as any).ethereum.request({ 
+          method: 'eth_requestAccounts' 
+        });
+        setLoginState(true)
+    }else{
+        alert("请安装MateMask钱包！")
+    }
+    
+}
+  
   return (
-    <div>index</div>
+    <div>
+      <header style={{width:"100%",height:"7%",backgroundColor:"gray",lineHeight:"7vh",fontSize:"2rem",padding:"0 1rem"}}>
+        <SplitText text="连接MetaMsk钱包" />
+      </header>
+      <main style={{width:"100%",height:"93vh",backgroundColor:"#f0f0f0",padding:'1rem'}}>
+        <button onClick={connectMetaMask} style={{height:'3rem',backgroundColor:"skyblue",padding:"8px"}}>连接MetaMask</button>
+        <div>
+          登录状态：{loginState?"已连接":"未连接"}
+        </div>
+      </main>
+    </div>
   )
 }
